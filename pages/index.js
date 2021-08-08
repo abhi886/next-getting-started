076c1fc36ef659171768ecef4da08a0052fb927c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-08 15:57:41 +1000

    Implement getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Alert from "../components/alert";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <>
       <Layout home>
@@ -17,10 +18,34 @@ export default function Home() {
             <a href='https://nextjs.org/learn'>our Next.js</a>.)
           </p>
         </section>
+        {/* Add this <section> tag below the existing <section> tag */}
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
       </Layout>
       <Alert type='error'>
         <p>This is alert Example</p>
       </Alert>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
