2a91e3e1b3605b4c46e5af8f8c67d448b341f7bb
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-08 13:05:23 +1000

    Use Classnames library to toggle classes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Alert from "../components/alert";
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi. I'm Abhishekh Maharjan.</p>
-        <p>
-          (This is a sample website I built on{" "}
-          <a href='https://nextjs.org/learn'>our Next.js</a>.)
-        </p>
-      </section>
-    </Layout>
+    <>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>Hi. I'm Abhishekh Maharjan.</p>
+          <p>
+            (This is a sample website I built on{" "}
+            <a href='https://nextjs.org/learn'>our Next.js</a>.)
+          </p>
+        </section>
+      </Layout>
+      <Alert type='error'>
+        <p>This is alert Example</p>
+      </Alert>
+    </>
   );
 }
